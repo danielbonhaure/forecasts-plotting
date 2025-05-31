ad443f7c8b11c488b1bff28f1de9a90ea4686628
Daniel Bonhaure <danielbonhaure@gmail.com>
2025-05-31 19:04:13 -0300

    Funcionalidad: se permite agregar información adicional a los mapas generados.
    Por ejemplo, esta funcionalidad permite indicar casos en los que algún modelo es excluído por problemas técnicos. Se implementó esta funcionalidad luego de que el IRI avisara que para el mes de mayo de 2025 el modelo CFSv2 tenía errores.

--- Helpers.R
+++ Helpers.R
@@ -615,11 +615,13 @@ PlotsHelper <- R6::R6Class(
         ".fa-eye {margin-right: 1px; width: 17px; height: 17px; background-image: url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 576 512'%3E%3Cpath d='M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM432 256c0 79.5-64.5 144-144 144s-144-64.5-144-144s64.5-144 144-144s144 64.5 144 144zM288 192c0 35.3-28.7 64-64 64c-11.5 0-22.3-3-31.6-8.4c-.2 2.8-.4 5.5-.4 8.4c0 53 43 96 96 96s96-43 96-96s-43-96-96-96c-2.8 0-5.6 .1-8.4 .4c5.3 9.3 8.4 20.1 8.4 31.6z'/%3E%3C/svg%3E\");}"
       css_fa_eye_slash <-
         ".fa-eye-slash {margin-bottom: -2px; width: 19px; height: 19px; background-image: url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 640 512'%3E%3Cpath d='M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zM223.1 149.5C248.6 126.2 282.7 112 320 112c79.5 0 144 64.5 144 144c0 24.9-6.3 48.3-17.4 68.7L408 294.5c5.2-11.8 8-24.8 8-38.5c0-53-43-96-96-96c-2.8 0-5.6 .1-8.4 .4c5.3 9.3 8.4 20.1 8.4 31.6c0 10.2-2.4 19.8-6.6 28.3l-90.3-70.8zm223.1 298L373 389.9c-16.4 6.5-34.3 10.1-53 10.1c-79.5 0-144-64.5-144-144c0-6.9 .5-13.6 1.4-20.2L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5z'/%3E%3C/svg%3E\");}"
+      css_info_adicional <-
+        "#info-adicional {font: small-caps 100%/200% serif; font-style: italic; font-weight: 1000; font-size: 90%; color: orangered;}"
       # Convert CSS to HTML
       html_fix <- htmltools::tags$style(
         type = "text/css", paste(css_fix_1, css_fix_2, css_fix_3, css_title, css_logos,
                                  css_logo_climar, css_logo_cima, css_logo_smn, css_logo_crcsas,
-                                 css_hide_button, css_fa_eye, css_fa_eye_slash))
+                                 css_hide_button, css_fa_eye, css_fa_eye_slash, css_info_adicional))
     },
     definir_javascript = function() {
       js <- "
@@ -871,6 +873,20 @@ PlotsHelper <- R6::R6Class(
                                  "\n{calibrated_prob_xtrm}.")
       } 
       
+      # Obtener información adicional del archivo de configuración
+      info_config <- global_config$get_config(base_file$type)$additional_info
+
+      # En caso de ser necesario, agregar info adicional al título generado
+      if ( info_config$display ) {
+        # Obtener información adicional
+        additional_info <- switch(lang, "en" = info_config$message$en,
+                                        "es" = info_config$message$es,
+                                        "pt" = info_config$message$pt)
+        # Agregar información adicional al título
+        main_title <- glue::glue(
+          "{main_title}\n<span id='info-adicional'>{additional_info}</span>")
+      }
+
       # Retornar título real
       return ( main_title )
       
